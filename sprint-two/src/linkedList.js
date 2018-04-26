2d55bfd947a4ee36314ca1c8e78d92c8b581fd3d
Eric Nague <ennague@gmail.com>
2018-04-26 16:12:36 -0700

    Pass first 3 tests of linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,8 +2,22 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  list.length = 0;
+
 
   list.addToTail = function(value) {
+    var new_Node = Node(value); //create new node
+
+    if( !list.head && !list.tail) { // if it is 1st node
+      list.head = new_Node; //1st node is head and tail
+      list.tail = new_Node;
+    } else {
+      list.tail.next = new_Node; //add node to the back
+      list.tail = list.tail.next; // reset tail to last node
+    }
+
+    list.length++; //increase length count
+
   };
 
   list.removeHead = function() {
@@ -27,3 +41,4 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+//LinkedList and Node are both constructors in the functional pattern
